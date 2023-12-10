a1a67452b336f0d43ce27687c96793182c4d6885
Derek Miller <derekjmiller@gmail.com>
2023-12-10 18:19:14 -0500

    add comment based help

--- public/Get-O365ManagementAvailableContent.ps1
+++ public/Get-O365ManagementAvailableContent.ps1
@@ -1,18 +1,48 @@
+<#
+.SYNOPSIS
+Lists content blobs (containers for logs) that are available to retrieve for the requested time frame.
+
+.DESCRIPTION
+Lists content blobs (containers for logs) that are available to retrieve for the requested time frame. Content blobs should be passed to Get-O365ManagementContent to retrieve the actual logs.
+
+.PARAMETER ContentType
+Choose an audit log type. Only one is supported at this time. Valid types are: Audit.AzureActiveDirectory, Audit.Exchange, Audit.SharePoint, Audit.General, DLP.All
+
+.PARAMETER StartDateTime
+The start of the requested time. May not be more than 7 days ago. Also, the API is not always precise in the time it retrieves.
+
+.PARAMETER EndDateTime
+The end of the requested time. This must be no more than 24 hours after the StartDateTime.
+
+.PARAMETER Uri
+Called internally. Only used to retrieve the next page of content blobs. Future releases will hide this parameter.
+
+.EXAMPLE 
+$contentBlob = Get-O365ManagementAvailableContent -ContentType Audit.AzureActiveDirectory
+#Retrieve content blob for last hour of AAD Audit logs.
+
+#>
 function Get-O365ManagementAvailableContent{
-    [cmdletbinding()]
+    [CmdletBinding()]
     param(
-        [parameter(Mandatory, ParameterSetName="Default")]
+        [parameter(Mandatory=$true, ParameterSetName="Default")]
         [ValidateSet("Audit.AzureActiveDirectory","Audit.Exchange","Audit.SharePoint","Audit.General","DLP.All")]
         [string]$ContentType,
         [Parameter(ParameterSetName="Default")]
         [datetime]$StartDateTime = (Get-Date).AddHours(-1),
         [Parameter(ParameterSetName="Default")]
         [datetime]$EndDateTime = (Get-Date),
-        [Parameter(ParameterSetName="Uri")]
+        [Parameter(Mandatory=$true, ParameterSetName="Uri")]
         [string]$Uri
     )
     $dateDiff = $EndDateTime - $StartDateTime
     if ($dateDiff -lt 0) {throw "StartDateTime must be before EndDateTime."}
+
+    $sevenDaysPastLimit = (Get-Date).AddDays(-7)
+    if ($StartDateTime -lt $sevenDaysPastLimit){
+        Write-Host -ForegroundColor Yellow "[WARN] " -NoNewline; Write-Host "Specified start date is more than 7 days in the past, shifting the start date to $sevenDaysPastLimit."
+        $StartDateTime = $sevenDaysPastLimit
+    }
 
     #The API requires no larger than a 24 hour window be specified
     #Running two Get-Dates can result in a time that is greater than 24 hours
